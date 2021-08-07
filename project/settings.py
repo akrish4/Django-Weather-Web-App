db34e96d034283823c316f9f653f118219ac362a
Ananthakrishnan Nair RS <61831021+akrish4@users.noreply.github.com>
2021-08-07 22:14:58 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -140,8 +140,8 @@ USE_L10N = True
 
 USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'i.am.dev.vijay@gmail.com'
-EMAIL_HOST_PASSWORD = 'ak@delhi'
+EMAIL_HOST_USER = 'yourpassword@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
